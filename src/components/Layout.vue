649220cf00615ea1f4858f079a0bd5766e4b6b1b
semanurakkuzu <semanurakkuzu@gmail.com>
2023-01-26 23:47:13 +0300

    add `Layout`

---
+++ src/components/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-layout>
+    <Sider></Sider>
+    <a-layout :style="{ marginLeft: '320px' }">
+      <Content></Content>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script>
+import Sider from './Sider.vue'
+import Content from './Content.vue'
+
+export default {
+  name: 'LayoutComponent',
+  components: {
+    Sider,
+    Content,
+  },
+}
+</script>
